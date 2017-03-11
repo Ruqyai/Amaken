13034c262a732b46118a32c602f6bc57ed22de25
zainah alloush <s130017905@gamil.com>
2017-03-11 04:39:57 +0300

    adding event recycle view

--- app/src/main/java/com/example/afaf/amakenapp/activities/BusinessProfileEvents.java
+++ app/src/main/java/com/example/afaf/amakenapp/activities/BusinessProfileEvents.java
@@ -2,17 +2,27 @@ package com.example.afaf.amakenapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.example.afaf.amakenapp.R;
-import com.example.afaf.amakenapp.activities.NavDrw;
+import com.example.afaf.amakenapp.helper.BusinessProfilePlaceOrEventAdapter;
+import com.example.afaf.amakenapp.helper.BusinessProfilePlaceOrEventListItem;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BusinessProfileEvents extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<BusinessProfilePlaceOrEventListItem> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,41 @@ public class BusinessProfileEvents extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.businessProfileEvents_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+
+            /* every item of  recycler view has a fixed size*/
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+            /* we put data we want to store inside the list item*/
+        listItems = new ArrayList<>();
+
+
+        for (int i = 0; i <= 10; i++) {
+            BusinessProfilePlaceOrEventListItem listItem = new BusinessProfilePlaceOrEventListItem(
+                    R.drawable.target,
+                    "Event Name " + i ,
+                    "Event Category",
+
+                    R.drawable.bookmark,
+                    R.drawable.redheart,
+                    R.attr.ratingBarStyleSmall,
+                    "21",
+                    "21",
+                    "21"
+            );
+
+            listItems.add(listItem);
+
+        }
+
+
+        adapter = new BusinessProfilePlaceOrEventAdapter(listItems, this);
+
+        recyclerView.setAdapter(adapter);
     }
 
 
