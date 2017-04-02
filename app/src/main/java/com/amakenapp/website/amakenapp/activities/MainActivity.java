9127f34db0b1a65b571837e9bf8c143390eff062
myprogra <myacount05@gmail.com>
2017-04-02 14:15:28 +0300

    facebook

--- app/src/main/java/com/amakenapp/website/amakenapp/activities/MainActivity.java
+++ app/src/main/java/com/amakenapp/website/amakenapp/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package com.amakenapp.website.amakenapp.activities;
 
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+
+
 
 import com.amakenapp.website.amakenapp.R;
 
