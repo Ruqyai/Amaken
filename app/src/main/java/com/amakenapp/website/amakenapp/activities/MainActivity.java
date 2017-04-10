ac11d416ab3e788c39966d4752909d3f5177a3c6
myprogra <myacount05@gmail.com>
2017-04-11 00:03:39 +0300

    fix the problem in sharedPreferences + login or sing up then fetch user data form database and store in it +logout and clear the data form it+recognize if user or business owner

--- app/src/main/java/com/amakenapp/website/amakenapp/activities/MainActivity.java
+++ app/src/main/java/com/amakenapp/website/amakenapp/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package com.amakenapp.website.amakenapp.activities;
 
+import android.content.Context;
 import android.content.Intent;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -10,19 +13,26 @@ import android.widget.ImageButton;
 
 
 import com.amakenapp.website.amakenapp.R;
+import com.amakenapp.website.amakenapp.helper.SharedPrefManager;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageButton signUpMain;
     private ImageButton signInMain;
+    private static Context mCtx;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-     ///   if (user!=null){} // TODO: 4/9/2017 here we should check if user already exist in cache memory if yes go to HomeActivity without sign up or sign in
-        startActivity(new Intent(MainActivity.this,NavDrw.class));
+       // SharedPreferences sharedPreferences = mCtx.getSharedPreferences("mysharedpref12", Context.MODE_PRIVATE);
+        SharedPrefManager sharedPrefManager=SharedPrefManager.getInstance(this);
+        if (sharedPrefManager.isLoggedIn()){
+            startActivity(new Intent(MainActivity.this,NavDrw.class));
+
+        } // TODO: 4/9/2017 here we should check if user already exist in cache memory if yes go to HomeActivity without sign up or sign in
+
         signUpMain= (ImageButton) findViewById(R.id.signUpMain);
         signInMain = (ImageButton) findViewById(R.id.signInMain);
 
