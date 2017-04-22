98b4e66a7e3f88f01a48842ea204e022a2b373e2
afaf <shoog-alroo7_@hotmail.com>
2017-04-23 00:25:45 +0300

    ....

---
+++ app/src/main/java/com/amakenapp/website/amakenapp/activities/BusinrssHelp.java
@@ -0,0 +1,15 @@
+package com.amakenapp.website.amakenapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amakenapp.website.amakenapp.R;
+
+public class BusinrssHelp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_businrss_help);
+    }
+}
